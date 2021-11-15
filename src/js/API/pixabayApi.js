b8b728c20adc192d35479d36a499598b5c468f37
Kuzia30 <maxzimka37@gmail.com>
2021-11-15 23:12:02 +0200

    fix load more

--- src/js/API/pixabayApi.js
+++ src/js/API/pixabayApi.js
@@ -1,10 +1,14 @@
 const BASE_URL = 'https://pixabay.com/api/';
 const KEY = '24347539-7a784c76778ec6b315780761f';
 let page = 1;
-
+let nameKey = '';
 
 export function fetchPictures(name) {
-   return fetch(`${BASE_URL}?key=${KEY}&per_page=40&page=${page}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true`)
+    if (nameKey !== name) {
+        nameKey = name;
+        page = 1;
+    }
+   return fetch(`${BASE_URL}?key=${KEY}&per_page=40&page=${page}&q=${nameKey}&image_type=photo&orientation=horizontal&safesearch=true`)
 .then(response => {
       if (response.ok) {
         return response.json();
