5dc544955908fe44bdaa97be41b6ef4f411aa3a6
Kuzia30 <maxzimka37@gmail.com>
2021-11-15 19:36:01 +0200

    add styles

--- src/js/API/pixabayApi.js
+++ src/js/API/pixabayApi.js
@@ -1,12 +1,13 @@
 const BASE_URL = 'https://pixabay.com/api/';
 const KEY = '24347539-7a784c76778ec6b315780761f';
 
-fetch(`${BASE_URL}?key=${KEY}&per_page=6&q=cat&image_type=photo&orientation=horizontal&safesearch=true`)
-// .then(response => {
-//       if (response.ok) {
-//         return response.json();
-//       }
-//       throw Error('Whoops');
-//     })
 
-// https://pixabay.com/api/?key=24347539-7a784c76778ec6b315780761f&q=yellow+flowers&image_type=photo
+export function fetchPictures(name) {
+   return fetch(`${BASE_URL}?key=${KEY}&per_page=40&page=1&q=${name}&image_type=photo&orientation=horizontal&safesearch=true`)
+.then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw Error('Whoops');
+    })
+}
--- src/js/pictures.js
+++ src/js/pictures.js
@@ -1,12 +1,43 @@
-import HTTPServise from './API/pixabayApi'
+import { fetchPictures } from './API/pixabayApi'
 
 const formEl = document.querySelector('#search-form');
-console.log(formEl);
+const galleryWrap = document.querySelector('.gallery');
+
+
 formEl.addEventListener('submit', onSubmitForm);
 function onSubmitForm(evt) {
     evt.preventDefault();
-  const formData = evt.currentTarget.elements.searchQuery.value;
-    console.log(formData);
-
+    const formData = evt.currentTarget.elements.searchQuery.value;
+    fetchPictures(formData).then(data => renderPictures(data.hits));
 }
+
+function renderPictures(pictures) {
+    const murkup = pictures.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => `<div class="photo-card">
+  <a class="gallery__item" href="${largeImageURL}">
+  <img src="${webformatURL}" alt="${tags}" loading="lazy"/>
+</a>
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>
+      ${likes}
+    </p>
+    <p class="info-item">
+      <b>Views</b>
+      ${views}
+    </p>
+    <p class="info-item">
+      <b>Comments</b>
+      ${comments}
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>
+      ${downloads}
+    </p>
+  </div>
+</div>`)
+        .join('');
+    
+    galleryWrap.innerHTML = murkup;
+}
+ 
 
